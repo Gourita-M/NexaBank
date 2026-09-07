3ad04ffbe01c6945aae3b5aeab1de2f133628003
Gourita-M <mouad.gourita@gmail.com>
2026-09-07 20:24:18 +0100

    Added Home Page System.out and a switch case

---
+++ src/main/java/org/example/nexabank/Home.java
@@ -0,0 +1,8 @@
+package org.example.nexabank;
+
+public class Home {
+    public void homePag()
+    {
+        
+    }
+}
--- src/main/java/org/example/nexabank/NexaBankApplication.java
+++ src/main/java/org/example/nexabank/NexaBankApplication.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class NexaBankApplication {
 
     public static void main(String[] args) {
-
+        
         Scanner scan = new Scanner(System.in);
         
         int choice = 0;
@@ -15,17 +15,26 @@ public class NexaBankApplication {
         System.out.println("Welcome to NexaBank");
         System.out.println("Login As: ");
 
-        choice = scan.nextInt();
+        System.out.println("1. Customer");
+        System.out.println("2. Manager");
 
+        boolean invalid = true;
 
-        switch (choice) {
-            case 1:
-                System.out.println("Monday");
-                break;
-        
-            default:
-                break;
+        while(invalid){
+            choice = scan.nextInt();
+            
+            switch (choice) {
+                case 1:
+                    System.out.println("Monday");
+                    invalid = false;
+                    break;
+            
+                default:
+                    System.out.println("Please Enter a Valid Number (1 - 2)");
+                    
+            }
         }
+        scan.close();
     }
 
 }
