471d9cb5ed478beefbf4adfe831e2abdeb0fe44c
Gourita-M <mouad.gourita@gmail.com>
2026-09-11 15:13:24 +0100

    refactor: Remove customer saving functionality and retain transaction history saving method

--- src/main/java/org/example/nexabank/model/Save.java
+++ src/main/java/org/example/nexabank/model/Save.java
@@ -3,57 +3,18 @@ package org.example.nexabank.model;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Save {
 
-    public void saveCustomers(HashMap<String, Customer> customers) {
+    public static void saveTransactionsHistory(String method,long accountNumber, int amount)
+    {
+        try {
+            BufferedWriter writeHistory = new BufferedWriter( new FileWriter("AccountsHistory.txt", true));
+                writeHistory.write(method + " | " + accountNumber + " | " + amount + " DH");
+                writeHistory.newLine();
 
-        try (BufferedWriter writer =
-                new BufferedWriter(new FileWriter("customers.txt"))) {
-
-            // Go through every Customer
-            for (Map.Entry<String, Customer> customerEntry
-                    : customers.entrySet()) {
-
-                String email = customerEntry.getKey();
-                Customer customer = customerEntry.getValue();
-
-                // Save Customer information
-                writer.write(
-                    "CUSTOMER|" +
-                    email + "|" +
-                    customer.getName()
-                );
-
-                writer.newLine();
-
-                // Go through all accounts belonging to this Customer
-                for (Map.Entry<Integer, Account> accountEntry
-                        : customer.getAccounts().entrySet()) {
-
-                    Integer accountId = accountEntry.getKey();
-                    Account account = accountEntry.getValue();
-
-                    // Save Account information
-                    writer.write(
-                        "ACCOUNT|" +
-                        accountId + "|" +
-                        account.getAccountType() + "|" +
-                        account.getAccountNumber() + "|" +
-                        account.getBalance()
-                    );
-
-                    writer.newLine();
-                }
-            }
-
-            System.out.println("Data saved successfully.");
-
+                writeHistory.close();
         } catch (IOException e) {
-
-            System.out.println("Error while saving data.");
             e.printStackTrace();
         }
     }
